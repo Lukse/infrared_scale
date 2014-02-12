408b94219894677806e290a9f30134568d714784
Saulius Lukse <saulius.lukse@gmail.com>
2014-02-12 11:04:54 +0200

    clean up code, smarter bitstream parsing

--- ir_scale_utils.py
+++ ir_scale_utils.py
@@ -1,25 +1,21 @@
+import bitstring
 import irtoy
 import serial
 import matplotlib.pyplot as plt
 
+
 def raw2int(text):
-	packet3 = 0
-	status = 0
+	t1i = 0
+	t2i = 0
 
 	if len(text) == 40:
-		k = 0
-		for j in range(15, 33):
-			if text[j] == '+':
-				packet3 = packet3 | (1 << (17-k))
-			k += 1
-
-		k = 0
-		for j in range(13, 15):
-			if text[j] == '+':
-				status = status | (1 << (1-k))
-			k += 1
-
-	return packet3, status
+		val = bitstring.BitArray(bin=text)
+		t1b = val[15:33].bin
+		t1i = bitstring.BitArray(bin=t1b).uint
+		t2b = val[13:15].bin
+		t2i = bitstring.BitArray(bin=t2b).uint
+		#print val.bin, t1i/10.0, t2i
+	return t1i, t2i
 
 
 def get_data(file):
@@ -28,13 +24,15 @@ def get_data(file):
 	f.close()
 	return lines
 
+
 def get_data_serial(port):
 	lines = []
 	with serial.Serial(port) as serialDevice:
 		print 'Prepare...'
 		toy = irtoy.IrToy(serialDevice)
-		print 'Reading IR signals...'
+		print 'Waiting for IR signals...'
 		raw_data = toy.receive()
+		print 'Got some data.'
 		i = 0
 		for tmp in range(0, len(raw_data) / 2):
 			value = (raw_data[i]*0x100+raw_data[i+1])*21.3333
@@ -47,40 +45,34 @@ def preprocess(lines):
 	data = []
 	for line in lines:
 		data.append(round(float(line),0))
-
 	packet = ''
 	packet2 = ''
 	packets = []
-
 	for i in range(0, len(data)):
 		if data[i] > 1100:
-
 			for j in range(0, len(packet)/2):
 				packet2 += packet[j*2]
 			packets.append(packet2)
-
 			packet = ''
 			packet2 = ''
-		
 		if data[i] < 800:
-			packet += '+'
-
+			packet += '1'
 		if data[i] > 800:
-			packet += '.'
+			packet += '0'
 	return packets
 
-def get_real_weight(packets):
+
+def get_real_weight(lines):
+	packets = preprocess(lines)
 	weights = []
 	for packet in packets:
 		raw_weight, status = raw2int(packet)
 		weight = float(raw_weight)/10.0
 		if status == 3:
 			weights.append(weight)
-
 	weight_sum = 0
 	for w in weights:
 		weight_sum += w
-
 	if len(weights) > 0:
 		avg_weight = weight_sum / len(weights)
 		return avg_weight
--- weight.py
+++ weight.py
@@ -1,8 +1,7 @@
 import ir_scale_utils
 
-lines = ir_scale_utils.get_data_serial('COM43')
+lines = ir_scale_utils.get_data('data/4-94,7kg.txt')
+#lines = ir_scale_utils.get_data_serial('COM43')
 
-packets = ir_scale_utils.preprocess(lines)
-weight = ir_scale_utils.get_real_weight(packets)
+weight = ir_scale_utils.get_real_weight(lines)
 print 'Your weight is: %3.1f kg' % weight
-
